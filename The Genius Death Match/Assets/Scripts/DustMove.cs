a8a95c537c3ba9d7487267c99f15ad089d311405
Wongoon <rmsgh64@gmail.com>
2025-10-03 01:14:19 +0900

    Add: `TMP Assets`

---
+++ The Genius Death Match/Assets/Scripts/DustMove.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DustMove : MonoBehaviour
+{
+    public Image dust;
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
